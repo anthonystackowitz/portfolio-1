40475d381c51cb7814392e3398d6ad6f90561ad3
anthonystackowitz <67646286+anthonystackowitz@users.noreply.github.com>
2020-07-19 13:20:11 -0400

    about update

--- pages/about.js
+++ pages/about.js
@@ -26,7 +26,7 @@ const About = () => {
           <Col md="6">
             <div className={`fadein`}>
               <p>
-                My name is Anthony Stachowitz and I am a developer who enjoys new technologies and likes to work on the edge.{" "}
+                My name is Anthony Stachowitz and I am a developer who enjoys new technologies and likes to work on the edge of tech.{" "}
               </p>
               <p>
                 I have a Bachelors degree in Criminal Justice, Political Science and Marketing from Metro State University of Denver. I studied full stack development at the Lambda school in San Francisco (online) and blockchain technology at the Emurgo Academy in India (online) - along with various self taught online classes.
